03a8b12bb0d87fb406b769316d5bdaa0b8be4256
gabieusa <gabrielaeusa@gmail.com>
2022-05-30 21:32:14 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const yourShip = document.querySelector('.player-shooter');
 const playArea = document.querySelector('#main-play-area');
-const aliensImg = ['img/monster-1.png', 'img/monster-2.png', 'img/monster-3.png'];
+const aliensImg = ['monster-1.png', 'monster-2.png', 'monster-3.png'];
 const instructionsText = document.querySelector('.game-instructions');
 const startButton = document.querySelector('.start-button');
 let alienInterval;
@@ -54,7 +54,7 @@ function createLaserElement() {
     let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
     let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
     let newLaser = document.createElement('img');
-    newLaser.src = 'img/shoot.png';
+    newLaser.src = 'shoot.png';
     newLaser.classList.add('laser');
     newLaser.style.left = `${xPosition}px`;
     newLaser.style.top = `${yPosition - 10}px`;
@@ -68,7 +68,7 @@ function moveLaser(laser) {
 
         aliens.forEach((alien) => { //comparando se cada alien foi atingido, se sim, troca o src da imagem
             if(checkLaserCollision(laser, alien)) {
-                alien.src = 'img/explosion.png';
+                alien.src = 'explosion.png';
                 alien.classList.remove('alien');
                 alien.classList.add('dead-alien');
             }
